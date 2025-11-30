71587233e0f6304e54cfc77e6de4f7428bb2c143
Yoseph Sagawa <yosephsagawa0@gmail.com>
2025-11-30 11:22:15 +0300

    Added the app level urls

--- backend/eld_app/views.py
+++ backend/eld_app/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+import requests
+from datetime import datetime, timedelta
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+OSRM_URL = "http://router.project-osrm.org"
+
+
